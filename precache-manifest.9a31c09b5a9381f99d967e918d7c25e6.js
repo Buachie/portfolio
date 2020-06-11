84d88daf417348e8292bc55b9a25951800184f3e
Aaron Buachie <aaron.buachiea@gmail.com>
2020-06-11 09:14:08 -0400

    Updates

---
+++ precache-manifest.9a31c09b5a9381f99d967e918d7c25e6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "168484e4f65b5ddc1544c10a6d7ec467",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5bd12adb7a34e2de5d2b",
+    "url": "/portfolio/static/css/main.782ecb79.chunk.css"
+  },
+  {
+    "revision": "5ff82aaa4ee379dce398",
+    "url": "/portfolio/static/js/2.822640ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.822640ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bd12adb7a34e2de5d2b",
+    "url": "/portfolio/static/js/main.5facd254.chunk.js"
+  },
+  {
+    "revision": "6d7034ed76a666b9f550",
+    "url": "/portfolio/static/js/runtime-main.1cff170f.js"
+  },
+  {
+    "revision": "5211ddaf1ff8f3d0d1c00c2e0415f7ea",
+    "url": "/portfolio/static/media/entertainment_sim.5211ddaf.png"
+  },
+  {
+    "revision": "504406c06ee3b7cc19140212f46b8e93",
+    "url": "/portfolio/static/media/heritage_trail.504406c0.png"
+  },
+  {
+    "revision": "1b66774c2ed6f2325458ad0c5bb53907",
+    "url": "/portfolio/static/media/test-img.1b66774c.jpg"
+  },
+  {
+    "revision": "0d16e291ef4d143d3868bcb417c25f62",
+    "url": "/portfolio/static/media/weather_app.0d16e291.png"
+  }
+]);
