8dd2333328f1aa497da6226c9747753a452398a4
Aaron Buachie <aaron.buachiea@gmail.com>
2021-08-06 15:12:42 -0400

    Update skills and portfolio projects

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,18 +9,18 @@ export class Hero extends Component {
         </h1>
         <div className="about">
           <p>
-            Hey there. My name is Aaron and I'm a web developer from Brooklyn, NY 
-            who is passionate and dedicated to his work and always open to learning 
-            new technologies and frameworks.
-            <br/>
-            <br/>
+            Hey there. My name is Aaron and I'm a web developer from Brooklyn,
+            NY who is passionate and dedicated to his work and always open to
+            learning new technologies and frameworks.
+            <br />
+            <br />
             Check out some of my work below.
           </p>
         </div>
         <div className="terminal-nav flex">
-            <div className="btn green"></div>
-            <div className="btn yellow"></div>
-            <div className="btn red"></div>
+          <div className="btn green"></div>
+          <div className="btn yellow"></div>
+          <div className="btn red"></div>
         </div>
         <div className="my-info">
           <div className="info-section">
@@ -30,26 +30,37 @@ export class Hero extends Component {
           <div className="info-section">
             <div className="info-title">Aaron.skills</div>
             <div className="info-data">
-              ["<span className="skill">HTML5</span>", "<span className="skill">CSS3</span>", "<span className="skill">Sass</span>", "<span className="skill">JavaScript</span>", "<span className="skill">ReactJS</span>", "<span className="skill">jQuery</span>", "<span className="skill">Wordpress</span>", "<span className="skill">Git</span>"]
+              ["<span className="skill">React</span>", "
+              <span className="skill">JavaScript</span>", "
+              <span className="skill">TypeScript</span>", "
+              <span className="skill">HTML5</span>", "
+              <span className="skill">CSS3</span>", "
+              <span className="skill">jQuery</span>", "
+              <span className="skill">Wordpress</span>", "
+              <span className="skill">Git</span>"]
             </div>
           </div>
           <div className="info-section">
             <div className="info-title">Aaron.contactInfo</div>
             <div className="info-data">
-              ["<a href="mailto:buachiea@gmail.com">buachiea@gmail.com</a>",{" "}
-              "<a href="https://www.linkedin.com/in/aaron-buachie-5b678775/">
+              ["<a href="mailto:buachiea@gmail.com">buachiea@gmail.com</a>", "
+              <a href="https://www.linkedin.com/in/aaron-buachie-5b678775/">
                 Linkedin
-              </a>"
-              , "<a href="https://github.com/Buachie">Github</a>"]
+              </a>
+              " , "<a href="https://github.com/Buachie">Github</a>"]
             </div>
           </div>
           <div className="info-section">
             <div className="info-title">Aaron.resume</div>
             <div className="info-data">
-              "<a href={Resume} rel="noopener noreferrer" target="_blank">Resume.pdf</a>"
+              "
+              <a href={Resume} rel="noopener noreferrer" target="_blank">
+                Resume.pdf
+              </a>
+              "
             </div>
           </div>
-          
+
           <div className="info-title">
             <span className="cursor">|</span>
           </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,79 +3,87 @@ import ProjectCard from "./ProjectCard";
 import Weather from "../img/weather_app.png";
 import Heritage from "../img/heritage_trail.png";
 import Entertainment from "../img/entertainment_sim.png";
-import Handbook from "../img/handbook.png"
-import ImageSearch from "../img/image_search.png"
+import Handbook from "../img/handbook.png";
+import ImageSearch from "../img/image_search.png";
+import Planets from "../img/planets.png";
+
 export class Projects extends Component {
   render() {
     return (
       <div className="projects-container" id="portfolio">
-          <div className="title-container">
-            <h1>Portfolio</h1>
-            <p>Check out some of the stuff I've worked on. </p>
-          </div>
-          <div className="card-container flex">
-            <ProjectCard
-              img={Weather}
-              title={"JavaScript Weather App"}
-              description={
-                "A JavaScript weather application that uses the Darksky API to provide the current weather and weekly forecast based on the user's current location. The background also changes to suit the time of day and weather."
-              }
-              source={"https://github.com/Buachie/weather-app"}
-              demo={"https://buachie.github.io/medp341/dist/assignments/Weather_App/index.html"}
-              skill1={"HTML5"}
-              skill2={"CSS3"}
-              skill3={"JavaScript"}
-              skill4={"ES6"}
-            />
-
-            <ProjectCard
-              img={ImageSearch}
-              title={"React Image Search"}
-              description={"An image searching tool built with ReactJS and Material UI that dynamically displays images using Pixabay's API along with the image tags and author."}
-              source={'https://github.com/Buachie/image-search'}
-              demo={"https://buachie-photo-search.netlify.app/"}
-              skill1={'JavaScript'}
-              skill2={'ES6'}
-              skill3={'ReactJS'}
-              skill4={'CSS3'}
-              skill5={'Material UI'}
-            />
-            <ProjectCard
-              img={Heritage}
-              title={"African American Heritage Trail"}
-              description={
-                "A site for the Upper Housatonic Valley African American Heritage Trail. I worked on this project as a junior front-end developer on a team where I helped customize the Wordpress 'Twenty Twenty' child theme."
-              }
-              demo={"https://www.africanamericantrail.org/"}
-              skill1={"HTML5"}
-              skill2={"CSS3"}
-              skill3={"Wordpress"}
-            />
-            <ProjectCard
-              img={Entertainment}
-              title={"Entertainment Simulator"}
-              description={
-                "A game made in React that provides the user with different things to do depending on the current weather. Choosing an option will affect various stats. All information is also stored locally for future use. This was my first React app and was a huge learning experience, especially in managing state and props."
-              }
-              source={"https://github.com/Buachie/entertainment-sim/"}
-              demo={"https://buachie.github.io/entertainment-sim/"}
-              skill1={"HTML5"}
-              skill2={"CSS3"}
-              skill3={"JavaScript"}
-              skill4={"ES6"}
-              skill5={"ReactJS"}
-            />
-            <ProjectCard
-              img={Handbook}
-              title= {"Hunter College Film Production Handbook"}
-              description={"An online handbook for the Hunter College Department of Film and Media allowing easy access to important forms and documents for students. This project involved working with department staff to update the old, outdated design"}
-              demo={"https://fm.hunter.cuny.edu/productionhandbook_draft/index.html"}
-              skill1={"HTML5"}
-              skill2={"CSS3"}
-            />
-            
-          </div>
+        <div className="title-container">
+          <h1>Portfolio</h1>
+          <p>Check out some of the stuff I've worked on. </p>
         </div>
+        <div className="card-container flex">
+          <ProjectCard
+            img={Weather}
+            title={"JavaScript Weather App"}
+            description={
+              "A JavaScript weather application that uses the Darksky API to provide the current weather and weekly forecast based on the user's current location. The background also changes to suit the time of day and weather."
+            }
+            source={"https://github.com/Buachie/weather-app"}
+            demo={
+              "https://buachie.github.io/medp341/dist/assignments/Weather_App/index.html"
+            }
+            skill1={"HTML5"}
+            skill2={"CSS3"}
+            skill3={"JavaScript"}
+            skill4={"ES6"}
+          />
+
+          <ProjectCard
+            img={ImageSearch}
+            title={"React Image Search"}
+            description={
+              "An image searching tool built with ReactJS and Material UI that dynamically displays images using Pixabay's API along with the image tags and author."
+            }
+            source={"https://github.com/Buachie/image-search"}
+            demo={"https://buachie-photo-search.netlify.app/"}
+            skill1={"JavaScript"}
+            skill2={"ES6"}
+            skill3={"ReactJS"}
+            skill4={"CSS3"}
+            skill5={"Material UI"}
+          />
+          <ProjectCard
+            img={Planets}
+            title={"Planet Facts"}
+            description={""}
+            source={"https://github.com/Buachie/planet-facts"}
+            demo={"https://planetoids.netlify.app/"}
+            skill1={"React"}
+            skill2={"TypeScript"}
+            skill3={"SCSS"}
+          />
+          <ProjectCard
+            img={Entertainment}
+            title={"Entertainment Simulator"}
+            description={
+              "A game made in React that provides the user with different things to do depending on the current weather. Choosing an option will affect various stats. All information is also stored locally for future use. This was my first React app and was a huge learning experience, especially in managing state and props."
+            }
+            source={"https://github.com/Buachie/entertainment-sim/"}
+            demo={"https://buachie.github.io/entertainment-sim/"}
+            skill1={"HTML5"}
+            skill2={"CSS3"}
+            skill3={"JavaScript"}
+            skill4={"ES6"}
+            skill5={"ReactJS"}
+          />
+          <ProjectCard
+            img={Handbook}
+            title={"Firechat"}
+            description={"A React application that allows users to "}
+            demo={
+              "https://fm.hunter.cuny.edu/productionhandbook_draft/index.html"
+            }
+            skill1={"TypeScript"}
+            skill2={"React"}
+            skill3={"Firebase"}
+            skill4={"React ContextAPI"}
+          />
+        </div>
+      </div>
     );
   }
 }
